25b1d550b553764dc94a326e6870dde14dd295d3
Camille Paviot /L3CDPN /2015 <camille.paviot@laposte.net>
2015-03-05 16:36:40 +0100

    Fin de l'tape 10 /Les formulaires

---
+++ src/Ens/JobeetBundle/Utils/Jobeet.php
@@ -0,0 +1,39 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Ens\JobeetBundle\Utils;
+class Jobeet
+{
+    static public function slugify($text)
+    {
+      // replace non letter or digits by -
+      $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+      // trim
+      $text = trim($text, '-');
+
+      // transliterate
+      if (function_exists('iconv'))
+      {
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+      }
+
+      // lowercase
+      $text = strtolower($text);
+
+      // remove unwanted characters
+      $text = preg_replace('#[^-\w]+#', '', $text);
+
+      if (empty($text))
+      {
+        return 'n-a';
+      }
+
+      return $text;
+    }
+}
