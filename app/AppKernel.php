271b7fa27ab781479c11fb7f6dab184ee2d6c4c7
Camille Paviot /L3CDPN /2015 <camille.paviot@laposte.net>
2015-03-29 20:12:57 +0200

    Fin de l'etape 12 /Le paquet Admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Ens\JobeetBundle\EnsJobeetBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
